c42992be8b8ca42afd21f9905660455d5254db8a
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-05-18 10:58:05 -0400

    Got the ongoing series

--- AnimeTrackerBot.py
+++ AnimeTrackerBot.py
@@ -3,44 +3,34 @@ import os
 import time
 import random
 import logging
-from discord.ext import commands
+from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
 import requests
 from privatekeylmao import token
 TOKEN = token
 
-"""
-#Actual interactions
-@client.event
-async def on_message(message):
-    
-    #to make sure it doesn't error
-    if message.author == client.user:
-        return
-    else:
-        m = str(message.content)
-        if message.content.startswith("-test"):
-
-            if m.find("top")!=-1:
-"""
-
+#-------Scraping-------
 def get_page(url):
     url = url
     data = requests.get(url)
     soup = BeautifulSoup(data.text,'lxml')
     return soup
 
+def get_list(soup):
+    newsoup = soup.find('div',class_ = "series")
+    llist = [[i.a["title"],i.a["href"]] for i in newsoup.find_all('li')]
+    return llist
+
 def get_episode(soup):
     episodes = soup.find('div', id = "stats")
-    print(episodes)
-    e = episodes.find_all('ul')
-    print(e)
     
+
+#-------Startup-------    
 client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
-    print("PH has logged in")
+    print("Anime has logged in")
     print(client.user.name)
     print(client.user.id)
     print("-----")
@@ -48,27 +38,42 @@ async def on_ready():
     for server in client.guilds:
         print(server.name)
     print("Bot Status Set")
+    check_list.start()
     await client.change_presence(activity = discord.Game(name = '!help for a list of commands'))
+
+
 class Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
         
-    @commands.command(help = "[name] Gets the user's watch list")
-    async def user(self,ctx,arg):
-        await ctx.channel.send("User {}'s list:".format(arg))
+    @commands.command(help = "Gets the user's watch list")
+    async def user(self,ctx):
+        await ctx.channel.send("User {}'s list:".format(ctx.author))
 
-    @commands.command(help = "[Username] [Anime Name] to add an anime to your watch list")
-    async def add(self,ctx,name,anime):
+    @commands.command(help = "[Anime Name] to add an anime to your watch list")
+    async def add(self,ctx,anime):
         #search and add the latest thing
         
         try:
             
-            soup = get_page(anime)
-            get_episode(soup)
             
-            await ctx.channel.send("User {}'s list will be updated to include {}.".format(name,anime))
+            
+            await ctx.channel.send("User {}'s list will be updated to include {}.".format(ctx.author,anime))
         except:
             await ctx.channel.send("Link failure")
+
+#-------Background Tasks-------
+@tasks.loop(minutes = 10)
+async def check_list():
+    print("Checking")
+    soup = get_page("https://gogoanime.io/")
+    anime_list = get_list(soup)
+
+    #Cross reference with current list
+
+    #then loop thru and get all da episodes
+    #anime_list = get_episode(soup)
+
         
 #To do:
 """
